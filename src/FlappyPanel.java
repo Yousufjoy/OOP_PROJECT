eddf5a9db7cef059c08e009862ef9231300e0a26
Yousufjoy <57608937+Yousufjoy@users.noreply.github.com>
2021-02-01 21:57:43 +0600

    Update FlappyPanel.java

--- src/FlappyPanel.java
+++ src/FlappyPanel.java
@@ -11,16 +11,18 @@ import java.awt.event.ActionListener;
 public class FlappyPanel extends JPanel implements KeyListener, ActionListener{
 
     final int WIDTH = 525, HEIGHT = 550;
+    int flappyHeight = HEIGHT / 4;
+    int flappyV = 0;
+    int flappyA = 8;
+    int flappyI = 1;
+    public static int score = 0;
     final int WALLXVELOCITY = 5;
     final int WALLWIDHT = 50;
-    int flappyHeight = HEIGHT / 4;
-    int flappyV = 0, flappyA = 8,flappyI=1; // velocity, Accelartion
-   public static int score = 0;
     int [] wallx = {WIDTH, WIDTH + WIDTH/2}; // for the wall
     int [] gap ={(int)(Math.random()*HEIGHT),(int)(Math.random()*HEIGHT)}; // for the gap between walls
 
 
-    boolean gameOver = false; 
+    boolean gameOver = false;
 
     Timer time = new Timer(40,this);
 
@@ -32,102 +34,22 @@ public class FlappyPanel extends JPanel implements KeyListener, ActionListener{
 
         setBackground(Color.CYAN);
 
-        //Adding frame
-       // new Timer(40,this).start();
     }
 
-    public void paintComponent(Graphics g){ // this will creat the bird
-        super.paintComponent(g); // If we don't we gonna notice the square appearing everywhere
-
-
-        if(!gameOver) {
-            g.setColor(Color.black);
-            g.drawString("SCORE "+ score,WIDTH/2,10);
-            drawWall(g);
-            logic();
-            drawFlappy(g);
-        } else {
-            g.setColor(Color.black);
-            g.drawString("SCORE "+ score,WIDTH/2,10);
-
-        }
-
-
-    }
-
-    public void actionPerformed(ActionEvent e){   // ActionEvent = When the user clicks a button
-
-
-        flappyA += flappyI;
-        flappyV += flappyA;
-        wallx[0] -= WALLXVELOCITY;
-        wallx[1] -= WALLXVELOCITY;
-
-        repaint();
-    }
-
-    private void drawFlappy(Graphics g){
+    private void drawFlappy(Graphics g){ // this will creat the bird
         g.setColor(Color.BLUE);
-
-        // Condition if we hit a wall we die
-
 
 
         g.fillRect(75,flappyHeight+flappyV,25,25);
     }
 
-    private void logic(){
 
-        for(int i=0; i<2; i++) {
-            if (wallx [i]<= 100 && wallx [i]+ WALLWIDHT >= 100 || wallx[i]<= 75 && wallx[i]+ WALLWIDHT>=75) {
-                if ((flappyHeight + flappyV) >= 0 && (flappyHeight + flappyV) <= gap[i]
-                        || ((flappyHeight + flappyV + 25) >= gap [i]+ 100 && (flappyHeight + flappyV + 25) <= HEIGHT)) {
-                    gameOver = true;
-                }
-            }
-
-            if(flappyHeight + flappyV <=0 || flappyHeight+flappyV +25 >=HEIGHT){
-                gameOver = true;
-            }
-
-            if(75 > wallx[i] + WALLWIDHT){
-                score++;
-            }
-
-            if(wallx[i] + WALLWIDHT <=0){
-
-                wallx[i] = WIDTH;
-                gap[i] = (int)(Math.random()*(HEIGHT-150));
-
-            }
-        }
-
-    }
-
-
-    private void drawWall(Graphics g){
-
-        for(int i=0; i<2; i++) {
-            g.setColor(Color.GRAY);
-
-            g.fillRect(wallx[i], 0, WALLWIDHT, HEIGHT);
-
-            g.setColor(Color.CYAN);
-            g.fillRect(wallx[i], gap[i], WALLWIDHT, 100);
-        }
-
-
-    }
-
-    public void keyTyped(KeyEvent e){  // KeyEvent = when a key is pressed, released, or typed.
-
-    }
 
     @Override
     public void keyPressed(KeyEvent e) {
         int code = e.getKeyCode();
 
-        // Setting acceleration
+        // Setting acceleration-going up
         if(code == e.VK_SPACE){
             flappyA = -8;
 
@@ -153,12 +75,98 @@ public class FlappyPanel extends JPanel implements KeyListener, ActionListener{
             gap  [1]  = (int)(Math.random()*HEIGHT-150);
 
 
-             gameOver = false;
+            gameOver = false;
 
-             repaint();
+          repaint();
 
         }
     }
+
+    public void actionPerformed(ActionEvent e){   // ActionEvent = When the user clicks a button
+
+
+        flappyV = flappyV + flappyA; // for every  frame there is we say because of this when we press space the bird is going up
+        flappyA = flappyA + flappyI;  //
+        wallx[0] = wallx[0]-WALLXVELOCITY;
+        wallx[1] = wallx[1]-WALLXVELOCITY;
+
+        repaint();
+    }
+
+    private void drawWall(Graphics g){
+
+        for(int i=0; i<2; i++) {
+            g.setColor(Color.GRAY);
+            g.fillRect(wallx[i], 0, WALLWIDHT, HEIGHT);
+
+            g.setColor(Color.CYAN);
+            g.fillRect(wallx[i], gap[i], WALLWIDHT, 100);
+        }
+
+
+    }
+
+
+
+    public void paintComponent(Graphics g){ // this will creat the bird
+        super.paintComponent(g); // If we don't we gonna notice the square appearing everywhere
+
+
+        if(!gameOver) {
+            g.setColor(Color.black);
+            g.drawString("SCORE "+ score,WIDTH/2,10);
+            drawWall(g);
+            logic();
+            drawFlappy(g);
+        } else {
+            g.setColor(Color.black);
+            g.drawString("SCORE "+ score,WIDTH/2,10);
+
+        }
+
+
+    }
+    
+
+    private void logic(){
+
+        for(int i=0; i<2; i++) {
+
+
+
+            // this dies if the flappy goes through
+            if(wallx[i] <=100 && wallx [i]+ WALLWIDHT >= 100){
+                if((flappyHeight+flappyV)>=0 && (flappyHeight+ flappyV)<= gap[i] || (flappyHeight+flappyV+25)>=gap[i]+100 && (flappyHeight+flappyV+25)
+                <=HEIGHT){
+                    gameOver = true;
+                }
+            }
+
+
+            if(flappyHeight + flappyV <=0 || flappyHeight+flappyV +25 >=HEIGHT){
+                gameOver = true;
+            }
+
+            if(75 > wallx[i] + WALLWIDHT){
+                score++;
+            }
+
+            if(wallx[i] + WALLWIDHT <=0){
+
+                wallx[i] = WIDTH;
+                gap[i] = (int)(Math.random()*(HEIGHT-150));
+
+            }
+        }
+
+    }
+
+
+
+    public void keyTyped(KeyEvent e){  // KeyEvent = when a key is pressed, released, or typed.
+
+    }
+
 
     @Override
     public void keyReleased(KeyEvent e) {
